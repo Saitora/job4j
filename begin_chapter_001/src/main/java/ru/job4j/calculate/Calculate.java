59128f7c9269628446d03473277d5ba8ca1d4e0d
Saitora <rost_1989@mail.ru>
2018-07-20 22:05:22 +0300

    added checkstyle plugin

--- begin_chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ begin_chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,10 +1,22 @@
 package ru.job4j.calculate;
 
-
-class Calculate {
+/**
+ * Class Calculate
+ * @author nikolaev r.a.
+ * @since 20.07.2018
+ */
+public class Calculate {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+
+    /**
+     * Prints "Echo, echo, echo: [name]
+     * @param name name to echo
+     */
+    public static void echo(String name) {
+        System.out.println("Echo, echo, echo: " + name);
     }
 
 }
---
+++ begin_chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Rostislav Nikolaev (mailto:rost_1989@mail.ru)
+ * @version 1.0
+ * @since 0.1
+ */
+package ru.job4j.calculate;
