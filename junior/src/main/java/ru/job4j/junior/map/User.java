5e93746da4cc894a3e4741e8b1c92f0354e3879b
Saitora <rost_1989@mail.ru>
2018-08-02 22:44:52 +0300

    task Map 1

---
+++ junior/src/main/java/ru/job4j/junior/map/User.java
@@ -0,0 +1,45 @@
+package ru.job4j.junior.map;
+
+import java.util.Calendar;
+
+public class User {
+
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, Calendar birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    public void setBirthday(Calendar birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+}
