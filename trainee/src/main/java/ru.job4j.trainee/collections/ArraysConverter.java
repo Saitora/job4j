9564ed93aec1fb0b393e0034068319ef0fa28dfb
Saitora <rost_1989@mail.ru>
2018-08-03 23:33:31 +0300

    [fix] smartprogress homework task_1

--- trainee/src/main/java/ru.job4j.trainee/collections/ArraysConverter.java
+++ trainee/src/main/java/ru.job4j.trainee/collections/ArraysConverter.java
@@ -3,19 +3,12 @@ package ru.job4j.trainee.collections;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ArraysConverter {
 
     public static List<Integer> convertMultiArrayToList(final int[][] array) {
-        return Arrays.stream(array).collect(
-                ArrayList::new,
-                (arrList, intArr) -> {
-                    for (int i : intArr) {
-                        arrList.add(i);
-                    }
-                },
-                ArrayList::addAll
-        );
+          return Arrays.stream(array).flatMapToInt(Arrays::stream).boxed().collect(Collectors.toList());
     }
 
 }
