3f5f8db5c59dd88285d26c9124e1b29c77be4e59
Saitora <rost_1989@mail.ru>
2018-08-29 23:36:41 +0300

    task waitnotify 3

--- junior/src/test/java/ru/job4j/junior/multithreading/waitnotify/SimpleBlockingQueueTest.java
+++ junior/src/test/java/ru/job4j/junior/multithreading/waitnotify/SimpleBlockingQueueTest.java
@@ -2,6 +2,13 @@ package ru.job4j.junior.multithreading.waitnotify;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.IntStream;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 public class SimpleBlockingQueueTest {
 
     @Test
@@ -21,6 +28,37 @@ public class SimpleBlockingQueueTest {
         prThread.join();
         conThread.interrupt();
         conThread.join();
+    }
+
+    @Test
+    public void whenProducedShouldBeFullyConsumed() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        Thread producer = new Thread(
+                () -> {
+                    IntStream.range(0, 5).forEach(
+                            queue::offer
+                    );
+                }
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer, is(Arrays.asList(0, 1, 2, 3, 4)));
     }
 
 }
